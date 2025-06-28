cd2ea205fbec6c05e0b2e35d65ce819d700e6ac3
joshinordee <inordeejoshua04@gmail.com>
2025-06-28 00:11:42 -0500

    drew triangle in the middle of screen, added player and circleshape files

--- main.py
+++ main.py
@@ -2,21 +2,30 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import*
+from constants import *
+from player import Player
 
 def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
+        # quit if game was exited
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # constrain frame rate to 60 fps
+        dt = clock.tick(60) / 1000   
     
-    #print("Starting Asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()

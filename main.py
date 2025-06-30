7bdac394f4d9193f9ed6365f6f02d80615639e02
joshinordee <inordeejoshua04@gmail.com>
2025-06-29 23:22:14 -0500

    Used groups to manage objects instead of directly using player

--- main.py
+++ main.py
@@ -13,14 +13,21 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         # quit if game was exited
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
 
         # constrain frame rate to 60 fps
